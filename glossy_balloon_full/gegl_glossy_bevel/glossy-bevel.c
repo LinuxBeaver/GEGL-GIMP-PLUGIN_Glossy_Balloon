ec7c1f4ce85927a394770592e1b6803a8242169a
LinuxBeaver <78667207+LinuxBeaver@users.noreply.github.com>
2023-05-02 13:20:40 -0400

    Update glossy-bevel.c
    Glossy Balloon has a checkbox to disable all puff around the edges. This also solves an issue in pango markup related to letter clipping.

--- glossy_balloon_full/gegl_glossy_bevel/glossy-bevel.c
+++ glossy_balloon_full/gegl_glossy_bevel/glossy-bevel.c
@@ -34,9 +34,18 @@ property_string (string, _("Graph1"), TUTORIAL)
 property_string (string2, _("Graph2"), TUTORIAL2)
     ui_meta     ("role", "output-extent")
 
-#define TUTORIAL2 \
-" id=3 screen aux=[   ref=3 emboss  type=bumpmap azimuth=30  elevation=15 ] median-blur  percentile=90 alpha-percentile=100 gaussian-blur std-dev-x=1 std-dev-y=1 filter=fir id=3 screen aux=[ ref=3  emboss  type=bumpmap azimuth=90  elevation=15 ] screen aux=[ ref=3  emboss  type=bumpmap azimuth=90  elevation=15 ] median-blur  percentile=50 alpha-percentile=100 screen aux=[ ref=3  emboss  type=bumpmap azimuth=90  elevation=15 ] median-blur  percentile=50 alpha-percentile=100 screen aux=[ ref=3  emboss  type=bumpmap azimuth=90  elevation=15 ]  id=forceopacity over  aux=[ ref=forceopacity ] id=makeopacity over  aux=[ ref=makeopacity ] id=forceopacity over  aux=[ ref=forceopacity ] id=makeopacity over  aux=[ ref=makeopacity ]  id=forceopacity over  aux=[ ref=forceopacity ] id=makeopacity over  aux=[ ref=makeopacity ] "\
+property_string (string3, _("Remove Transparency"), TUTORIAL3)
+    ui_meta     ("role", "output-extent")
 
+
+
+
+#define TUTORIAL2 \
+" id=3 screen aux=[   ref=3 emboss  type=bumpmap azimuth=30  elevation=15 ] median-blur  percentile=90 alpha-percentile=100 gaussian-blur std-dev-x=1 std-dev-y=1 filter=fir id=3 screen aux=[ ref=3  emboss  type=bumpmap azimuth=90  elevation=15 ] screen aux=[ ref=3  emboss  type=bumpmap azimuth=90  elevation=15 ] median-blur  percentile=50 alpha-percentile=100 screen aux=[ ref=3  emboss  type=bumpmap azimuth=90  elevation=15 ] median-blur  percentile=50 alpha-percentile=100 screen aux=[ ref=3  emboss  type=bumpmap azimuth=90  elevation=15 ]  "\
+
+
+#define TUTORIAL3 \
+" id=forceopacity over  aux=[ ref=forceopacity ] id=makeopacity over  aux=[ ref=makeopacity ] id=forceopacity over  aux=[ ref=forceopacity ] id=makeopacity over  aux=[ ref=makeopacity ]  id=forceopacity over  aux=[ ref=forceopacity ] id=makeopacity over  aux=[ ref=makeopacity ] id=forceopacity over  aux=[ ref=forceopacity ] "\
 
 
 property_double (gaus, _("Balloonification of text"), 6.0)
@@ -61,32 +70,17 @@ property_double (saturation, _("Desaturation for Image File Upload"), 1.2)
 property_file_path(src, _("Image file overlay (Desaturation and bright light recommended)"), "")
     description (_("Source image file path (png, jpg, raw, svg, bmp, tif, ...)"))
 
-property_double (opacityall, _("Slide up to remove transparency puff around edges"), 1.0)
+property_double (opacityall, _("Reduce transparency puff around edges"), 5.0)
     description (_("Global opacity value that is always used on top of the optional auxiliary input buffer."))
-    value_range (1, 2)
-    ui_range    (1.0, 2)
+    value_range (1, 5.0)
+    ui_range    (1.0, 5.0)
+
+property_boolean (removetransparency, _("Entirely Remove transparent puff on edges"), FALSE)
+  description    (_("In Pango Markup mode this will clip the borders of letters. So disable it there."))
 
 
 
-property_double (opacity, _("Opacity of image file overlay"), 1.0)
-    description (_("Global opacity value that is always used on top of the optional auxiliary input buffer."))
-    value_range (0, 1.0)
-    ui_range    (0.0, 1.0)
-    ui_meta     ("role", "output-extent")
 
-property_double (sharpen, _("Sharpen (only works on image overlays)"), 0.0)
-    description(_("Scaling factor for unsharp-mask, the strength of effect"))
-    value_range (0.0, 2.5)
-    ui_range    (0.0, 2.5)
-    ui_gamma    (3.0)
-    ui_meta     ("role", "output-extent")
-
-property_int (radius1, _("Blur to smooth edges (works best for image file overlays)"), 0)
-   description(_("A mild box blur to smooth rough edges"))
-   value_range (1, 2)
-   ui_range    (1, 2)
-   ui_gamma   (1.5)
-    ui_meta     ("role", "output-extent")
 
 
 
@@ -99,10 +93,30 @@ property_int (radius1, _("Blur to smooth edges (works best for image file overla
 
 #include "gegl-op.h"
 
+typedef struct
+{
+  GeglNode *input;
+  GeglNode *nop;
+  GeglNode *graph;
+  GeglNode *blur;
+  GeglNode *graph2;
+  GeglNode *hue;
+  GeglNode *saturation;
+  GeglNode *multiply;
+  GeglNode *opacityall;
+  GeglNode *layer;
+  GeglNode *removetransparency;
+  GeglNode *output;
+} State; 
+
+
+
+
 static void attach (GeglOperation *operation)
 {
   GeglNode *gegl = operation->node;
-  GeglNode *input, *output, *blur, *graph, *graph2, *hue, *box, *layer, *opacity, *sharpen, *saturation, *opacityall, *multiply;
+  GeglProperties *o = GEGL_PROPERTIES (operation);
+  GeglNode *input, *output, *blur, *graph, *graph2, *removetransparency, *nop, *hue,  *layer, *saturation, *opacityall, *multiply;
 
   input    = gegl_node_get_input_proxy (gegl, "input");
   output   = gegl_node_get_output_proxy (gegl, "output");
@@ -120,16 +134,9 @@ static void attach (GeglOperation *operation)
                                   "operation", "gegl:gegl",
                                   NULL);
 
-
-
- box   = gegl_node_new_child (gegl,
-                                  "operation", "gegl:box-blur",
+ removetransparency   = gegl_node_new_child (gegl,
+                                  "operation", "gegl:gegl",
                                   NULL);
-
- sharpen   = gegl_node_new_child (gegl,
-                                  "operation", "gegl:unsharp-mask",
-                                  NULL);
-
 
  hue   = gegl_node_new_child (gegl,
                                   "operation", "gegl:hue-chroma",
@@ -139,9 +146,7 @@ static void attach (GeglOperation *operation)
                                   "operation", "gegl:layer",
                                   NULL);
 
- opacity   = gegl_node_new_child (gegl,
-                                  "operation", "gegl:opacity",
-                                  NULL);
+
 
  opacityall   = gegl_node_new_child (gegl,
                                   "operation", "gegl:opacity",
@@ -158,41 +163,80 @@ static void attach (GeglOperation *operation)
                                   "operation", "gegl:multiply",
                                   NULL);
 
-
-
-
-  gegl_node_link_many (input, graph, blur, graph2, box, hue, saturation, multiply, opacityall, output, NULL);
-  gegl_node_connect_from (multiply, "aux", sharpen, "output");
-  gegl_node_link_many (layer, sharpen, NULL);
+ nop   = gegl_node_new_child (gegl,
+                                  "operation", "gegl:nop",
+                                  NULL);
 
 
 
   gegl_operation_meta_redirect (operation, "gaus", blur, "std-dev-x");
   gegl_operation_meta_redirect (operation, "gaus", blur, "std-dev-y");
-  gegl_operation_meta_redirect (operation, "radius1", box, "radius");
   gegl_operation_meta_redirect (operation, "hue", hue, "hue");
   gegl_operation_meta_redirect (operation, "lightness", hue, "lightness");
   gegl_operation_meta_redirect (operation, "src", layer, "src");
   gegl_operation_meta_redirect (operation, "saturation", saturation, "scale");
   gegl_operation_meta_redirect (operation, "string",  graph, "string");
   gegl_operation_meta_redirect (operation, "string2",  graph2, "string");
-  gegl_operation_meta_redirect (operation, "sharpen",  sharpen, "scale");
+  gegl_operation_meta_redirect (operation, "string3",  removetransparency, "string");
   gegl_operation_meta_redirect (operation, "opacityall",  opacityall, "value");
 
 
 
 
+  gegl_node_link_many (input, graph, blur, graph2, hue, saturation, multiply, opacityall, output, NULL);
+  gegl_node_connect_from (multiply, "aux", layer, "output");
+  gegl_node_link_many (layer, nop,  NULL);
 
+
+ /* Now save points to the various gegl nodes so we can rewire them in
+   * update_graph() later
+   */
+  State *state = g_malloc0 (sizeof (State));
+  state->input = input;
+  state->graph = graph;
+  state->blur = blur;
+  state->graph2 = graph2;
+  state->hue = hue;
+  state->saturation = saturation;
+  state->multiply = multiply;
+  state->opacityall = opacityall;
+  state->layer = layer;
+  state->nop = nop;
+  state->removetransparency = removetransparency;
+  state->output = output;
+  o->user_data = state;
 }
+
+static void
+update_graph (GeglOperation *operation)
+{
+  GeglProperties *o = GEGL_PROPERTIES (operation);
+  State *state = o->user_data;
+  if (!state) return;
+
+  if (o->removetransparency)
+  {
+  gegl_node_link_many (state->input, state->graph, state->blur, state->graph2, state->removetransparency, state->hue, state->saturation, state->multiply,  state->output, NULL);
+  gegl_node_connect_from (state->multiply, "aux", state->layer, "output");
+  gegl_node_link_many (state->layer, state->nop, NULL);
+  }
+  else
+  {
+  gegl_node_link_many (state->input, state->graph, state->blur, state->graph2, state->hue, state->saturation, state->multiply, state->opacityall,  state->output, NULL);
+  gegl_node_connect_from (state->multiply, "aux", state->layer, "output");
+  gegl_node_link_many (state->layer, state->nop, NULL);
+  }
+}
+
 
 static void
 gegl_op_class_init (GeglOpClass *klass)
 {
-  GeglOperationClass *operation_class;
-
-  operation_class = GEGL_OPERATION_CLASS (klass);
+  GeglOperationClass *operation_class = GEGL_OPERATION_CLASS (klass);
+   GeglOperationMetaClass *operation_meta_class = GEGL_OPERATION_META_CLASS (klass);
 
   operation_class->attach = attach;
+  operation_meta_class->update = update_graph;
 
   gegl_operation_class_set_keys (operation_class,
     "name",        "gegl:glossy-balloon",
