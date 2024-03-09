e98bbc4ceb227f4c73caf1b33242a009e7cbc476
LinuxBeaver <78667207+LinuxBeaver@users.noreply.github.com>
2024-03-09 17:38:20 -0500

    Update glossy-bevel-.c
    glossy balloon was given clip-extent=false to comply with CMYK Student's Gimp build.

--- source_code_of_glossy_balloon/glossy-bevel-.c
+++ source_code_of_glossy_balloon/glossy-bevel-.c
@@ -23,11 +23,11 @@
  
 color-overlay value=#f587ff median-blur  percentile=90 alpha-percentile=100 
 median-blur  percentile=90 alpha-percentile=290
-gaussian-blur std-dev-x=4 std-dev-y=4
+gaussian-blur clip-extent=false std-dev-x=4 std-dev-y=4
 id=3
 screen aux=[ ref=3  emboss  type=bumpmap azimuth=90  elevation=15 ] 
 median-blur  percentile=90 alpha-percentile=290
-gaussian-blur std-dev-x=4 std-dev-y=4
+gaussian-blur clip-extent=false std-dev-x=4 std-dev-y=4
 id=3
 screen aux=[ ref=3  emboss  type=bumpmap azimuth=90  elevation=15 ] 
 median-blur  percentile=90 alpha-percentile=290
@@ -60,7 +60,7 @@ the point in fixing meaningless errors.*/
 
 
 #define TUTORIAL2 \
-" id=3 screen aux=[   ref=3 emboss  type=bumpmap azimuth=30  elevation=15 ] median-blur  percentile=90 alpha-percentile=100 gaussian-blur std-dev-x=1 std-dev-y=1 filter=fir id=3 screen aux=[ ref=3  emboss  type=bumpmap azimuth=90  elevation=15 ] screen aux=[ ref=3  emboss  type=bumpmap azimuth=90  elevation=15 ] median-blur  percentile=50 alpha-percentile=100 screen aux=[ ref=3  emboss  type=bumpmap azimuth=90  elevation=15 ] median-blur  percentile=50 alpha-percentile=100 screen aux=[ ref=3  emboss  type=bumpmap azimuth=90  elevation=15 ]  "\
+" id=3 screen aux=[   ref=3 emboss  type=bumpmap azimuth=30  elevation=15 ] median-blur  percentile=90 alpha-percentile=100 gaussian-blur clip-extent=false std-dev-x=1 std-dev-y=1 filter=fir id=3 screen aux=[ ref=3  emboss  type=bumpmap azimuth=90  elevation=15 ] screen aux=[ ref=3  emboss  type=bumpmap azimuth=90  elevation=15 ] median-blur  percentile=50 alpha-percentile=100 screen aux=[ ref=3  emboss  type=bumpmap azimuth=90  elevation=15 ] median-blur  percentile=50 alpha-percentile=100 screen aux=[ ref=3  emboss  type=bumpmap azimuth=90  elevation=15 ]  "\
 /* This GEGL graph is a instruction for emboss to fuss with screen like a normal bevel but done five times over*/
 
 
@@ -199,7 +199,7 @@ GeglProperties *o = GEGL_PROPERTIES (operation);
   output   = gegl_node_get_output_proxy (gegl, "output");
 
   blur    = gegl_node_new_child (gegl,
-                                  "operation", "gegl:gaussian-blur",
+                                  "operation", "gegl:gaussian-blur clip-extent=false",
    "filter", 1,
                                   NULL);
 /* Filter one is code for Gaussian Blur Fir*/
